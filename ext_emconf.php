fcd1302ab156b0116573eb7c7dfafc3cc8a82453
sonawane332 <rutuja.sonawane017@gmail.com>
2024-08-02 14:44:17 +0530

    Added logo

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,12 +9,13 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '8.7.30-12.4.99',
         ],
     ],
-    'version' => '1.0.1',
+    'version' => '1',
     'icon' => 'EXT:auth0_sso/Resources/Public/Icons/miniorange.svg',
     'state' => 'stable',
     'autoload' => [
         'psr-4' => [
             'Miniorange\\Auth0SSO\\' => 'Classes/',
         ],
-    ]
+    ],
+    'icon' => 'EXT:keycloak_sso/Resources/Public/Icons/miniorange.png'
 ];
